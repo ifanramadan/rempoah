fe9db6d07eb48ccafd38b9e5b11fbb5df908f194
ifanramadan <cipanramadan5@gmail.com>
2024-12-10 21:57:20 +0700

    Create Kesehatan_25_style.js

---
+++ styles/Kesehatan_25_style.js
@@ -0,0 +1,39 @@
+var size = 0;
+var placement = 'point';
+
+var style_Kesehatan_25 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "7.800000000000001px \'Arial Black\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "#fafafa";
+    var bufferWidth = 1.0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("Nama") !== null) {
+        labelText = String(feature.get("Nama"));
+    }
+    var style = [ new ol.style.Style({
+        image: new ol.style.Icon({
+                  imgSize: [579.997, 579.997],
+                  scale: 0.037931230678779376,
+                  anchor: [11, 11],
+                  anchorXUnits: "pixels",
+                  anchorYUnits: "pixels",
+                  rotation: 0.0,
+                  src: "styles/cross.svg"
+            }),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
